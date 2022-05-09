6e5f886f6013325e6edfe28a6140dd4a9f3d6039
Jeremy Venditto <jeremy@venditto.info>
2022-05-09 16:17:56 -0400

    fixes

--- testing.sh
+++ testing.sh
@@ -1,4 +1,11 @@
-n# Laptop or desktop?
+#!/bin/bash
+
+# My Arch Linux Install
+
+			##############
+			### PROMPT ###
+			##############
+
 PS3='Please enter your choice: '
 options=("Laptop" "Desktop" "Virtual Machine" "Quit")
 select opt in "${options[@]}"
@@ -23,6 +30,10 @@ do
     esac
 done
 
+			##################
+			### ESSENTIALS ###
+			##################
+
 # Install Git
 sudo pacman -S git
 # Make folder named jeremy-venditto in the home folder
@@ -31,46 +42,25 @@ mkdir -p ~/jeremy-venditto && cd ~/jeremy-venditto
 git clone https://github.com/jeremy-venditto/bash-scripts
 git clone https://github.com/jeremy-venditto/dotfiles
 git clone https://github.com/jeremy-venditto/wallpaper
-# Enable nano syntax highlighting
-~/jeremy-venditto/nano-syntax-highlighting.sh
-# Move config files
-   #files
-mv ~/jeremy-venditto/dotfiles/.bash_profile /~
-mv ~/jeremy-venditto/dotfiles/.bashrc ~/
-mv ~/jeremy-venditto/dotfiles/.profile ~/
-mv ~/jeremy-venditto/dotfiles/.xinitrc ~/
-mv ~/jeremy-venditto/dotfiles/.xprofile ~/
-#echo "Sudo required for moving files to /usr/share/pixmaps/"
-sudo mv ~/jeremy-venditto/dotfiles/usr/share/pixmaps/* /usr/share/pixmaps/
-   #directories
-mv ~/jeremy-venditto/dotfiles/.config/ ~/
-mv ~/jeremy-venditto/dotfiles/.local/ ~/
-      #wallpaper
-	#wallpaper directory? default is ~/
-mv ~/jeremy-venditto/wallpaper/ ~/
 
-#      #lightdm config
-#sudo mv /etc/lightdm/lightdm.conf lightdm.conf_backup
-#if [[ $MACHINE = "LAPTOP" ]] || [[ $MACHINE = "VIRTUAL" ]]; then
-#sudo mv ~/jeremy-venditto/dotfiles/etc/lightdm/lightdm.conf_laptop /etc/lightdm/lightdm.conf
-#else sudo mv ~/jeremy-venditto/dotfiles/etc/lightdm/lightdm.conf_desktop /etc/lightdm/lightdm.conf;fi
+			##################
+			#### PACKAGES ####
+			##################
 
-
-### INSTALL PACKAGES
 function LAPTOP_PACKAGES {
-sudo pacman -S - < ~/jeremy-venditto/dotfiles/.resources/pacman.txt
-sudo pacman -S - < ~/jeremy-venditto/dotfiles/.resources/laptop-only.txt
+sudo pacman -S - < ~/jeremy-venditto/dotfiles/.resources/pacman_laptop.txt
+#sudo pacman -S - < ~/jeremy-venditto/dotfiles/.resources/aur_laptop.txt
 git clone https://aur.archlinux.org/yay
 cd yay && makepkg -si
-yay -S - < ~/jeremy-venditto/dotfiles/.resources/aur-laptop.txt
+yay -S - < ~/jeremy-venditto/dotfiles/.resources/aur_laptop.txt
 }
 
 function DESKTOP_PACKAGES {
-sudo pacman -S - < ~/jeremy-venditto/dotfiles/.resources/pacman.txt
-sudo pacman -S - < ~/jeremy-venditto/dotfiles/.resources/laptop-only.txt
+sudo pacman -S - < ~/jeremy-venditto/dotfiles/.resources/pacman_desktop.txt
+#sudo pacman -S - < ~/jeremy-venditto/dotfiles/.resources/aur_desktop.txt
 git clone https://aur.archlinux.org/yay
 cd yay && makepkg -si
-yay -S - < ~/jeremy-venditto/dotfiles/.resources/aur-desktop.txt
+yay -S - < ~/jeremy-venditto/dotfiles/.resources/aur_desktop.txt
 }
 
 function VIRTUAL_PACKAGES {
@@ -81,8 +71,9 @@ cd yay && makepkg -si
 yay -S - < ~/jeremy-venditto/dotfiles/.resources/aur-vm.txt
 }
 
+	### INTSALL PACKAGES ###
 #sudo pacman -S autoconf make gcc perl fakeroot automake
-sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort ~/jeremy-venditto/dotfiles/.resources/packages/all.txt))
+#sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort ~/jeremy-venditto/dotfiles/.resources/packages/all.txt))
 if [[ $MACHINE = "DESKTOP" ]]; then DESKTOP_PACKAGES;fi
 if [[ $MACHINE = "LAPTOP" ]]; then LAPTOP_PACKAGES;fi
 if [[ $MACHINE = "VIRTUAL" ]]; then VIRTUAL_PACKAGES;fi
@@ -91,19 +82,19 @@ if [[ $MACHINE = "VIRTUAL" ]]; then VIRTUAL_PACKAGES;fi
 ### install dmenu
 cd ~/.config/dmenu && sudo make install
 
+			###################
+			### EDIT CONFIG ###
+			###################
 
 ## Screen Resolution
-cd ~/jeremy-venditto/
-echo "xrandr --output Virtual-1 --primary --mode 1024x768 --rate 60" > screen-normal.sh
-echo "xrandr --output Virtual-1 --primary --mode 1920x1080 --rate 60" > screen-full.sh
-chmod +x screen*
-
+echo "xrandr --output Virtual-1 --primary --mode 1024x768 --rate 60" > ~/jeremy-venditto/screen-normal.sh
+echo "xrandr --output Virtual-1 --primary --mode 1920x1080 --rate 60" > ~/jeremy-venditto/screen-full.sh
+chmod +x ~/jeremy-venditto/screen*
 
 # Change Nitrogen Settings
 if [[ $MACHINE = DESKTOP ]]; then echo 'soon';fi
 if [[ $MACHINE = LAPTOP ]]; then echo 'soon';fi
 if [[ $MACHINE = VIRTUAL ]]; then sed -i "/DIRS=/c\DIRS=/home/"$USER"/wallpaper/1920x1080" ~/.config/nitrogen/nitrogen.cfg;fi
-
 
 # Change LightDM settings
 if [[ $MACHINE = DESKTOP ]]; then echo 'soon';fi
@@ -112,6 +103,25 @@ if [[ $MACHINE = VIRTUAL ]]; then sudo cp ~/jeremy-venditto/dotfiles/etc/lightdm
 
 # Change Grub Wallpaper
 sudo sed -i "/#GRUB_BACKGROUND=/c\GRUB_BACKGROUND=/home/"$USER"/wallpaper/grub/004-1024x768" /etc/default/grub
+
+# Enable nano syntax highlighting
+~/jeremy-venditto/nano-syntax-highlighting.sh
+
+# Move config files
+  #files
+mv ~/jeremy-venditto/dotfiles/.bash_profile /~
+mv ~/jeremy-venditto/dotfiles/.bashrc ~/
+mv ~/jeremy-venditto/dotfiles/.profile ~/
+mv ~/jeremy-venditto/dotfiles/.xinitrc ~/
+mv ~/jeremy-venditto/dotfiles/.xprofile ~/
+#echo "Sudo required for moving files to /usr/share/pixmaps/"
+sudo mv ~/jeremy-venditto/dotfiles/usr/share/pixmaps/* /usr/share/pixmaps/
+  #directories
+mv ~/jeremy-venditto/dotfiles/.config/ ~/
+mv ~/jeremy-venditto/dotfiles/.local/ ~/
+    #wallpaper
+    #wallpaper directory? default is ~/
+mv ~/jeremy-venditto/wallpaper/ ~/
 
 #### TO DO #####
 # Make rc.lua_virtual
