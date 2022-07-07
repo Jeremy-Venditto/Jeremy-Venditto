58c6c6808949553e26d710ea4ba984c46f8f0003
Jeremy Venditto <jeremy@venditto.info>
2022-07-07 19:26:46 -0400

    fix

--- arch-install.sh
+++ arch-install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+#     __                         _   __            ___ __  __
+# __ / /__ _______ __ _  __ __  | | / /__ ___  ___/ (_) /_/ /____
+#/ // / -_) __/ -_)  ' \/ // /  | |/ / -_) _ \/ _  / / __/ __/ _ \
+#\___/\__/_/  \__/_/_/_/\_, /   |___/\__/_//_/\_,_/_/\__/\__/\___/
+#                      /___/
+
+#Jeremy Venditto
+#https://github.com/jeremy-venditto
+#https://jeremyvenditto.info
+
 # My Arch Linux Install
-
-#automated answers
-#printf '%s\n' y | arch-install.sh
-
 #----------------------------------------------------
+
+	#########################
+	### Script Automation ###
+	#########################
+
 function AUTOMATED_ALL {
 read -r -p "Automate Script? [Y/n] " input ; case $input in
     [yY][eE][sS]|[yY]) AUTOMATED=YES ;;
@@ -27,8 +38,80 @@ y
 n
 EOF
 ~/jeremy-venditto/automated_vm.txt | ~/jeremy-venditto/arch-install.sh
+#automated answers
+#printf '%s\n' y | arch-install.sh
 }
 
+
+function DISKS_PARTITIONS {
+
+# View Disks before modifying
+lsblk
+# Select Installation Hard Disk
+read -rp "INSTALL DRIVE: " -e -i /dev/ INSTALL_DRIVE
+#cfdisk $INSTALL_DRIVE
+
+read -r -p "Coninue with disk formatting? [Y/n] " input ; case $input in
+    [yY][eE][sS]|[yY])
+
+# Partition Drive (manual as of now)
+cfdisk $INSTALL_DRIVE # Make this automated
+
+
+
+# Create EFI Partition (not mounted)
+read -rp "EFI PARTITION: " -e -i /dev/ EFI_PART
+echo $EFI_PART > efi.txt # EFI_PART variable is lost after arch-chroot
+mkfs.fat -F32 $EFI_PART > /dev/null
+echo "EFI Partition created on $EFI_PART"
+
+# Create Swap Partition
+read -rp "SWAP PARTITION: " -e -i /dev/ SWAP_PART
+echo $SWAP_PART > swap.txt # SWAP_PART variable is lost after arch-chroot
+mkswap $SWAP_PART && swapon $SWAP_PART
+if [[ -z $SWAP_PART ]]; then echo "SWAP file set up on (edit me)"; else echo "SWAP space set up on $SWAP_PART";fi
+        ## SWAPFILE
+#allocate -l 3G /swapfile
+#chmod 600 /swapfile
+#mkswap /swapfile
+#swapon /swapfile
+#echo '/swapfile none swap sw 0 0' >> /etc/fstab
+#free -m
+
+
+# Create Root Partition mounted to /mnt
+read -rp "ROOT PARTITION: " -e -i /dev/ ROOT_PART
+mkfs.ext4 $ROOT_PART
+mount $ROOT_PART /mnt > /dev/null
+echo "ROOT PARTITION mounted on $ROOT_PART"
+
+# Create Home Partition mounted to /mnt/home
+read -rp "HOME PARTITION: " -e -i /dev/ HOME_PART
+mkfs.ext4 $HOME_PART
+mkdir -p /mnt/home
+mount $HOME_PART /mnt/home > /dev/null
+
+# Assign Computer Hostname to system
+read -rp "Machine Hostname: " HOSTNAME
+echo $HOSTNAME > hostname.txt
+
+echo
+echo "Install Drive: $INSTALL_DRIVE"
+echo "EFI Partiton: $EFI_PART"
+echo "SWAP Partition: $SWAP_PART"
+echo "Root Partition: $ROOT_PART"
+echo "Home Partition: $HOME_PART"
+echo "Hostname: $HOSTNAME"
+;;
+    [nN][oO]|[nN]) echo "No"      ;; *) echo "Invalid input...";exit 1;;esac
+
+}
+
+
+
+        ###############################################
+        ### Arch ISO environment before arch-chroot ###
+        ###############################################
 
 function ARCH_ISO {
 
@@ -54,120 +137,49 @@ if [[ $? = 0 ]]; then echo 'Internet = Yes'; else echo 'Internet = No';fi
 echo 'Setting Network Time Protocol'
 timedatectl set-ntp true
 
-
-	#### FIX ME #######################!!!!!
-
-###### MANUAL CONFIGURATION #######
-
-# View Disks before modifying
-lsblk
-# Select Installation Hard Disk
-read -rp "INSTALL DRIVE: " -e -i /dev/ INSTALL_DRIVE
-#cfdisk $INSTALL_DRIVE
-# Create EFI Partition (not mounted)
-read -rp "EFI PARTITION: " -e -i /dev/ EFI_PART
-echo $EFI_PART > efi.txt
-#mkfs.fat -F32 $EFI_PART > /dev/null
-#echo "EFI Partition created on $EFI_PART"
-# Create Swap Partition (don't swap on yet)
-read -rp "SWAP PARTITION: " -e -i /dev/ SWAP_PART
-echo $SWAP_PART > swap.txt
-#mkswap $SWAP_PART > /dev/null
-# Create Root Partition mounted to /mnt
-read -rp "ROOT PARTITION: " -e -i /dev/ ROOT_PART
-#mkfs.ext4 $ROOT_PART
-#mount $ROOT_PART /mnt > /dev/null
-#echo "ROOT PARTITION mounted on $ROOT_PART"
-# Create Home Partition mounted to /mnt/home
-read -rp "HOME PARTITION: " -e -i /dev/ HOME_PART
-#mkfs.ext4 $HOME_PART
-#mkdir /mnt/home
-#mount $HOME_PART /mnt/home > /dev/null
-#echo "HOME PARTITION mounted on $HOME_PART"
-
-read -rp "Machine Hostname: " HOSTNAME
-echo $HOSTNAME > hostname.txt
-echo
-echo "Install Drive: $INSTALL_DRIVE"
-echo "EFI Partiton: $EFI_PART"
-echo "SWAP Partition: $SWAP_PART"
-echo "Root Partition: $ROOT_PART"
-echo "Home Partition: $HOME_PART"
-read -r -p "Coninue with disk formatting? [Y/n] " input ; case $input in
-    [yY][eE][sS]|[yY])
-
-# Partition Drive (manual as of now)
-cfdisk $INSTALL_DRIVE # Make this automated
-
-# Create EFI Partition (not mounted)
-mkfs.fat -F32 $EFI_PART > /dev/null
-echo "EFI Partition created on $EFI_PART"
-	# Create Swap Space (Partition or file (don't swap on yet))
-#	mkswap $SWAP_PART > /dev/null
-
-# Create Swap
-mkswap $SWAP_PART && swapon $SWAP_PART
-
-if [[ -z $SWAP_PART ]]; then echo "SWAP file set up on (edit me)"; else echo "SWAP space set up on $SWAP_PART";fi
-# Create Root Partition mounted to /mnt
-mkfs.ext4 $ROOT_PART
-mount $ROOT_PART /mnt > /dev/null
-echo "ROOT PARTITION mounted on $ROOT_PART"
-# Create Home Partition mounted to /mnt/home
-
-
-mkfs.ext4 $HOME_PART
-mkdir -p /mnt/home
-mount $HOME_PART /mnt/home > /dev/null ;;
-    [nN][oO]|[nN]) echo "No"      ;; *) echo "Invalid input...";exit 1;;esac
-lsblk
+# Set Disk/Partitions
+DISKS_PARTITIONS
 read -r -p "Are the Partitions Correct? [Y/n] " input ; case $input in
-    [yY][eE][sS]|[yY]) echo "Yes" ;;
-    [nN][oO]|[nN]) echo "No"      ;; *) echo "Invalid input...";exit 1;;esac
-
-
-#sgdisk -Z ${DISK} # zap all on disk
-#sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
-
-# create partitions
-#sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
-#sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-#sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
-
-
-	### END FIX ME #####################!!!!!
-
-# temp fix
-#echo 'Formatting and mounting disks'
-#mkfs.fat -F32 /dev/vda1
-#mkfs.ext4 /dev/vda3 && mount /dev/vda3 /mnt
-#mkfs.ext4 /dev/vda4 && mkdir -p /mnt/home && mount /dev/vda4 /mnt/home
+    [yY][eE][sS]|[yY]) echo "Yes" > /dev/null ;;
+    [nN][oO]|[nN]) DISKS_PARTITIONS ;; *) echo "Invalid input...";exit 1;;esac
 
 # Install System
 echo 'Installing base system'
 pacstrap -i /mnt base linux linux-firmware sudo nano curl
 echo 'System Installed'
+
 # Generate File System Table
 echo 'Generating File System Table'
 genfstab -U -p /mnt > /mnt/etc/fstab
+#genfstab -U -p /mnt > /mnt/etc/fstab
 echo "created /etc/fstab"
-# Move install file into /mnt
-echo "Install Script moved to /mnt"
+
+# Move files into /mnt so we can use them later
 mv arch-install.sh /mnt/
-#mv swap.txt /mnt/
+echo "Install Script moved to /mnt"
+# Moving lost user input variables so we can use them again
 mv efi.txt /mnt/
 mv hostname.txt /mnt/
+#mv swap.txt /mnt/
+
 # Chroot into system
 echo "Chrooting into system"
 arch-chroot /mnt /bin/bash /arch-install.sh -a
 }
 
+        #############################################
+        ### Arch ISO enviroment after arch-chroot ###
+        #############################################
+
+# Chrooting into /mnt stopped the script, so I needed another function
+
 function ISO_AFTER_CHROOT {
+
 #SWAP_PART=$(cat swap.txt)
 EFI_PART=$(cat efi.txt)
 HOSTNAME=$(cat hostname.txt)
-
 #Chrooting into /mnt stopped the script. so here we are with another function
+
 echo 'Generating locales'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
@@ -177,21 +189,36 @@ echo 'Setting Time Zone'
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc --utc
 echo 'Time Zone Configured'
+
+# Set Hostname
 echo 'Creating Hostname'
-#USERHOSTNAME=archvm
-#echo $USERHOSTNAME > /etc/hostname
 echo $HOSTNAME > /etc/hostname
 echo "127.0.1.1 localhost.localdomain $USERHOSTNAME" > /etc/hosts
 echo "Hostname set as $USERHOSTNAME"
 
+# Update and Install NetworkManager, Grub and EfiBootMgr
 echo 'Updating System'
 pacman -Sy
 echo 'Installing NetworkManager Grub and EfiBootMgr'
 pacman -S networkmanager grub efibootmgr --noconfirm
 systemctl enable NetworkManager
 echo 'NetworkManager has been enabled'
+
+# Set Root Password
 echo;echo 'set root password...'
 passwd
+
+# Create User
+echo 'Creating User'
+USERUSERNAME="arch-user"
+useradd -m -g users -G wheel -s /bin/bash $USERUSERNAME
+echo;echo 'Set User Password'
+passwd $USERUSERNAME
+echo 'User Creation Complete'
+  # Add user to wheel group for sudo privlidges
+  echo 'Adding user to wheel group'
+  echo "%wheel ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo
+
 # EFI
 echo 'Creating EFI partition'
 mkdir /boot/efi
@@ -199,46 +226,19 @@ mount $EFI_PART /boot/efi
 echo 'EFI partition created and mounted on /boot/efi'
 #lsblk # to check if everything is mounted correctly
 
+# Grub Bootloader
 echo 'Installing grub'
-#read
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --removable
-echo 'Grub installed..'
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --removable && echo 'Grub installed..'
 echo 'Creating Grub Config file'
 grub-mkconfig -o /boot/grub/grub.cfg
 echo 'Grub Config created'
-#read
-	## SWAPFILE
-#allocate -l 3G /swapfile
-#chmod 600 /swapfile
-#mkswap /swapfile
-#swapon /swapfile
-#echo '/swapfile none swap sw 0 0' >> /etc/fstab
-#free -m
 
-## Swao Partition
 
-#mkswap $SWAP_PART && swapon $SWAP_PART
-#mkswap /dev/vda2 && swapon /dev/vda2
-
-## USER
-#read USERNAME
-echo 'Creating User'
-USERUSERNAME="arch-user"
-useradd -m -g users -G wheel -s /bin/bash $USERUSERNAME
-echo;echo 'Set User Password'
-passwd $USERUSERNAME
-echo 'User Creation Complete'
-#EDITOR=nano visudo
-
-# Add user to wheel group for sudo privlidges
-echo 'Adding user to wheel group'
-echo "%wheel ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo
-
-#sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers && echo 'User now has sudo privledges' || echo 'bad'
 # add cron job here or something to boot script at next login
 mv /arch-install.sh /home/$USERUSERNAME/ && chown arch-user /home/arch-user/arch-install.sh
-#mv /arch-install.sh /home/$USERUSERNAME/ && chmod arch-user /home/arch-user/arch-install.sh
+echo
 echo 'You may now reboot your system'
+echo 'Run this script again at next boot'
 }
 
 #----------------------------------------------------
@@ -426,9 +426,5 @@ do
 done
 
 #### TO DO #####
-# Make rc.lua_virtual
-# Make /etc/lightdm/lightdm.conf_virtual
 # Make wallpaper directory switcher script
-# Make xrandr script or something idk maybe .bashr
 # Enable sudo privlidges in tty
-# Fix /etc/sudoers issue
