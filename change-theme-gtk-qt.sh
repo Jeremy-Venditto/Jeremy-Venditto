3c4cccf23cffd38fc0d7092c529a3d30a93bff71
Jeremy Venditto <jeremy@venditto.info>
2022-07-30 22:30:07 -0400

    fix

--- change-theme-gtk-qt.sh
+++ change-theme-gtk-qt.sh
@@ -17,13 +17,32 @@
 # adwaita adwaida-dark obsidian-icons-red kanada-cursors papirus-icons
 # i will add links here later
 
+#md5sums
+ # gtk 2
+#c1c6d014f7b058bfbe951b2f6750d3b2  gtkrc-2.0_dark
+#e2991a83c7f5bd426baf20488fd8257c  gtkrc-2.0_light
+ # gtk 3
+#f70e9c707bb04409181a203456f94c72  settings.ini_dark
+#6961a11d537725307fd7ea8bce05919b  settings.ini_light
+ # .xresources
+#9683a96313de0fad9851a5074618b7dc  Xresources-dark
+#9d90a6f112a2d07e865a7bb175ac4eeb  Xresources-light
+ # qt5
+#490d1ff5097b69b9125002840f3e3c91  qt5ct.conf_dark
+#30a772f38d2ce04bd31f328afbbf4289  qt5ct.conf_light
+
+#gtk3sum
+#gtk3sum
+#qt5ctsum_sum
+#xresources_sum
+
 function Main {
 
 if [[ -e "$HOME/.config/gtk-2.0/gtkrc-2.0_light" ]]; then
   echo "file exists" > /dev/null;else
 echo "~/.config/gtk-2.0/gtkrc-2.0_light does not exist, downloading file"
 curl -O https://raw.githubusercontent.com/jeremy-venditto/dotfiles/main/.config/gtk-2.0/gtkrc-2.0_light
-mv gtkrc-2.0_light ~/.config/gtkrc-2.0/
+mv gtkrc-2.0_light ~/.config/gtk-2.0/
 fi
 
 if [[ -e "$HOME/.config/gtk-2.0/gtkrc-2.0_dark" ]]; then
@@ -32,7 +51,6 @@ echo "~/.config/gtk-2.0/gtkrc-2.0_dark does not exist, downloading file"
 curl -O https://raw.githubusercontent.com/jeremy-venditto/dotfiles/main/.config/gtk-2.0/gtkrc-2.0_dark
 mv gtkrc-2.0_dark ~/.config/gtk-2.0/
 fi
-
 
 if [[ -e "$HOME/.config/gtk-3.0/settings.ini_dark" ]]; then
   echo "file exists" > /dev/null;else
@@ -83,7 +101,7 @@ fi
 }
 
 function Xterm {
-XTERM_THEME=$(cat .Xresources | grep foreground | cut -d ':' -f2 | cut -d ' ' -f2)
+XTERM_THEME=$(cat ~/.Xresources | grep foreground | cut -d ':' -f2 | cut -d ' ' -f2)
 if [[ $XTERM_THEME = black ]]; then cp ~/.config/xterm/Xreources-light ~/.Xresources;fi
 if [[ $XTERM_THEME = white ]]; then cp ~/.config/xterm/Xreources-dark ~/.Xresources;fi
 xrdb ~/.Xresources
@@ -112,18 +130,20 @@ function HELP_FUNCTION {
 while getopts ":dhl" option; do
    case $option in
       d) # Change to Dark Mode
-         cp ~/github/dotfiles/.gtkrc-2.0_dark ~/.gtkrc-2.0
-         cp ~/github/dotfiles/.config/gtk-3.0/settings.ini_dark ~/.config/gtk-3.0/settings.ini
-         cp ~/github/dotfiles/.config/qt5ct/qt5ct.conf_dark ~/.config/qt5ct/qt5ct.conf
+         cp ~/.config/gtk-2.0/gtkrc-2.0_dark ~/.gtkrc-2.0
+         cp ~/.config/gtk-3.0/settings.ini_dark ~/.config/gtk-3.0/settings.ini
+         cp ~/.config/qt5ct/qt5ct.conf_dark ~/.config/qt5ct/qt5ct.conf
+         cp ~/.config/xterm/Xreources-light ~/.Xresources
          exit;;
       h) # Print Help
          HELP_FUNCTION
          exit;;
       l) # Change to Light Mode
-         cp ~/github/dotfiles/.gtkrc-2.0_light ~/.gtkrc-2.0
-         cp ~/github/dotfiles/.config/gtk-3.0/settings.ini_light ~/.config/gtk-3.0/settings.ini
-         cp ~/github/dotfiles/.config/qt5ct/qt5ct.conf_light ~/.config/qt5ct/qt5ct.conf
-         exit;;
+         cp ~/.config/gtk-2.0/gtkrc-2.0_light ~/.gtkrc-2.0
+         cp ~/.config/gtk-3.0/settings.ini_light ~/.config/gtk-3.0/settings.ini
+         cp ~/.config/qt5ct/qt5ct.conf_light ~/.config/qt5ct/qt5ct.conf
+         cp ~/.config/xterm/Xreources-light ~/.Xresources
+exit;;
      \?) # Invalid option
          echo "Error: Invalid option"
          exit;;
